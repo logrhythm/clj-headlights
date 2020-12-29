cc3f85cacfe9e789cceb5911faf22910237ea86e
Mike Keesey <mike.keesey@logrhythm.com>
2020-12-29 16:30:51 -0700

    2.11.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject logrhythm/clj-headlights "2.11.0"
+(defproject logrhythm/clj-headlights "2.11.1-SNAPSHOT"
   :description "Clojure on Beam"
   :url "https://github.com/logrhythm-oss/clj-headlights"
   :license {:name "Apache License, Version 2.0"
